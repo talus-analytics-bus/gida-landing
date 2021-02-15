6effde61a2d30f0569bd9e93f7fb7cfed7e6b594
mvanmaele <mvanmaele@talusanalytics.com>
2021-02-15 11:58:14 -0500

    Update AMP data and update script

--- update-amp.sh
+++ update-amp.sh
@@ -3,16 +3,16 @@ echo Updating AMP XLSX download, please ensure AMP API server is running locally
 cd .src/downloads;
 curl -X POST "http://localhost:8001/post/export?class_name=All_data_recreate" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"filters\":{}}" > "COVID AMP - Policy and Plan Data Export.xlsx";
 
-# Rebuild
-echo Rebuilding project...;
+# Deploy
+echo Deploying project...;
 cd ../../;
-npm run build;
+npm run deploy;
 
-# Sync to S3
-echo Syncing to S3...;
-cd ./build;
-aws s3 sync . s3://gida-landing --acl public-read --delete; aws cloudfront create-invalidation \
---distribution-id E3EBPTUIIJ9IOB \
---paths "/*";
+# # Sync to S3
+# echo Syncing to S3...;
+# cd ./build;
+# aws s3 sync . s3://idea-landing --acl public-read --delete; aws cloudfront create-invalidation \
+# --distribution-id E3EBPTUIIJ9IOB \
+# --paths "/*";
 
 echo Update complete!
