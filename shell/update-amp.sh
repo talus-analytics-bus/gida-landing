948445041bee521c75ab141e8520656724b2d3d1
mvanmaele <mvanmaele@talusanalytics.com>
2021-07-07 15:45:37 -0400

    Reorg. shell scripts

---
+++ shell/update-amp.sh
@@ -0,0 +1,6 @@
+# Update AMP XLSX download and push everything live
+echo Updating AMP XLSX download, please ensure AMP API server is running locally first...;
+cd ./src/downloads;
+curl -X POST "http://localhost:8002/post/export?class_name=All_data_recreate" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"filters\":{}}" -o "COVID AMP - Policy and Plan Data Export.xlsx";
+open "COVID AMP - Policy and Plan Data Export.xlsx";
+echo Review and save XLSX file, then do `npm run deploy` to update site;
