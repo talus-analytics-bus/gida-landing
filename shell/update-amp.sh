89d87106051dc4afa55174f661c96d7e342a5de1
mvanmaele <mvanmaele@talusanalytics.com>
2022-01-19 10:09:33 -0500

    Update AMP update scripts

--- shell/update-amp.sh
+++ shell/update-amp.sh
@@ -2,5 +2,5 @@
 echo Updating AMP XLSX download, please ensure AMP API server is running locally first...;
 cd ./src/downloads;
 curl -X POST "http://localhost:8002/post/export?class_name=All_data_recreate" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"filters\":{}}" -o "COVID AMP - Policy and Plan Data Export.xlsx";
-open "COVID AMP - Policy and Plan Data Export.xlsx";
-echo "Review and save XLSX file, then do 'npm run deploy' to update site";
+# open "COVID AMP - Policy and Plan Data Export.xlsx";
+# echo "Review and save XLSX file, then do 'npm run deploy' to update site";
