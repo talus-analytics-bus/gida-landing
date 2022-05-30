246866468924343124f9d61e1413b40454b50e50
Ryan <rzimmerman@talusanalytics.com>
2022-05-30 19:04:41 -0300

    Adding project logos hook

---
+++ src/cmsHooks/useProjectLogosData.ts
@@ -0,0 +1,32 @@
+import { useStaticQuery, graphql } from 'gatsby'
+
+import { AirtableCMSData } from '@talus-analytics/library.airtable-cms'
+
+const useProjectLogosData = () => {
+  const { cmsProjectLogosData }: { cmsProjectLogosData: AirtableCMSData } =
+    useStaticQuery(graphql`
+      {
+        cmsProjectLogosData: allAirtable(
+          filter: { table: { eq: "Project Logos" } }
+        ) {
+          nodes {
+            data {
+              Name
+              Text
+              Image {
+                localFiles {
+                  childImageSharp {
+                    gatsbyImageData(height: 300, placeholder: BLURRED)
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `)
+
+  return cmsProjectLogosData
+}
+
+export default useProjectLogosData
