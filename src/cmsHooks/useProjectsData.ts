07179c09641ae336bf62e40500cbf513f76ad3da
Ryan <rzimmerman@talusanalytics.com>
2022-05-30 18:40:56 -0300

    Adding projectData query

---
+++ src/cmsHooks/useProjectsData.ts
@@ -0,0 +1,60 @@
+import { useStaticQuery, graphql } from 'gatsby'
+import {
+  FileNode,
+  IGatsbyImageDataParent,
+} from 'gatsby-plugin-image/dist/src/components/hooks'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
+
+export interface ProjectData {
+  Name: string
+  Text: string
+  Image: {
+    localFiles: FileNode[] &
+      { childImageSharp: IGatsbyImageDataParent<IGatsbyImageData> }[]
+  }
+  Link: string
+  Logo_Name: {
+    data: {
+      name: string
+    }
+  }[]
+}
+
+export interface ProjectsData {
+  nodes: {
+    data: ProjectData
+  }[]
+}
+
+const useProjectsData = () => {
+  const { projectsData }: { projectsData: ProjectsData } =
+    useStaticQuery(graphql`
+      {
+        projectsData: allAirtable(filter: { table: { eq: "Projects" } }) {
+          nodes {
+            data {
+              Name
+              Text
+              Link
+              Logo_Name {
+                data {
+                  Name
+                }
+              }
+              Image {
+                localFiles {
+                  childImageSharp {
+                    gatsbyImageData(height: 400, placeholder: BLURRED)
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `)
+
+  return projectsData
+}
+
+export default useProjectsData
