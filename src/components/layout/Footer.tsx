b03a50d1b91d6efe92d50b8ca82a23ce7fe86044
Ryan <rzimmerman@talusanalytics.com>
2022-05-30 19:20:10 -0300

    Making footer its own component

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Footer = () => (
+  <div className="footer home">
+    <div className="content">
+      <a target="_blank" href="https://ghssidea.org">
+        <img
+          src="assets/logo-title.png"
+          alt="International Disease and Events Analysis"
+        />
+      </a>
+      <a target="_blank" href="https://ghss.georgetown.edu/">
+        <img
+          src="assets/logo-georgetown.png"
+          alt="Georgetown University Center for Global Health Science and Security"
+        />
+      </a>
+      <a
+        target="_blank"
+        href="http://talusanalytics.com/"
+        className="talus-logo"
+      >
+        <img src="assets/logo-talus.png" alt="Talus Analytics, LLC" />
+        <div className="built-by">Built by</div>
+      </a>
+    </div>
+  </div>
+)
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Main from '../components/layout/Main'
 import useIndexPageData from '../cmsHooks/useIndexPageData'
 import HomepageNav from 'components/layout/Nav/HomepageNav'
 import Cards from 'components/landingPage/Cards/Cards'
+import Footer from 'components/layout/Footer'
 
 import '../components/layout/oldStaticSiteStyles/index.scss'
 
@@ -30,7 +31,7 @@ const IndexPage = (): JSX.Element => {
             dangerouslySetInnerHTML={{
               __html: CMS.getText(data, 'Intro text'),
             }}
-          ></p>
+          />
         </div>
         <div className="cards">
           <h1 className="no-top-margin">
@@ -39,30 +40,7 @@ const IndexPage = (): JSX.Element => {
           <Cards />
         </div>
       </Main>
-      <div className="footer home">
-        <div className="content">
-          <a target="_blank" href="https://ghssidea.org">
-            <img
-              src="assets/logo-title.png"
-              alt="International Disease and Events Analysis"
-            />
-          </a>
-          <a target="_blank" href="https://ghss.georgetown.edu/">
-            <img
-              src="assets/logo-georgetown.png"
-              alt="Georgetown University Center for Global Health Science and Security"
-            />
-          </a>
-          <a
-            target="_blank"
-            href="http://talusanalytics.com/"
-            className="talus-logo"
-          >
-            <img src="assets/logo-talus.png" alt="Talus Analytics, LLC" />
-            <div className="built-by">Built by</div>
-          </a>
-        </div>
-      </div>
+      <Footer />
     </Providers>
   )
 }
