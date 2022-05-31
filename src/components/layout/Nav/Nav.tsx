d1354920cc86156bf9b6ec5beaf61ffcdb2f6dab
Ryan <rzimmerman@talusanalytics.com>
2022-05-30 21:18:51 -0300

    Adding nav bar for other pages

---
+++ src/components/layout/Nav/Nav.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'gatsby'
+import React from 'react'
+
+const Nav = () => (
+  <nav className="navbar">
+    <div className="wrapper">
+      <Link className="nav-logo-container" to="/">
+        <img src="assets/small-logo.png" alt="IDEA logo" className="nav-logo" />
+      </Link>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link activeClassName="active" to="/about">
+          About
+        </Link>
+        <Link activeClassName="active" to="/contact">
+          Contact
+        </Link>
+      </div>
+    </div>
+  </nav>
+)
+
+export default Nav
