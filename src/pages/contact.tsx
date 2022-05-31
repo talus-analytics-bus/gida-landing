76f4cdf6d4b7cecacf017d96532f4ae140bcc6e9
Ryan <rzimmerman@talusanalytics.com>
2022-05-30 21:19:00 -0300

    Adding Nav to contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,14 @@
 import CMS from '@talus-analytics/library.airtable-cms'
 import Contact from 'components/Contact/Contact'
 import Footer from 'components/layout/Footer/Footer'
+import Nav from 'components/layout/Nav/Nav'
 import Providers from 'components/layout/Providers'
 import React from 'react'
 
 const ContactPage = () => (
   <Providers>
     <CMS.SEO />
+    <Nav />
     <Contact />
     <Footer />
   </Providers>
