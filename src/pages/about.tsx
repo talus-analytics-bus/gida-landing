e617867ccce69076b755c9210689a1c1f9d3fafb
Ryan <rzimmerman@talusanalytics.com>
2022-05-30 21:19:06 -0300

    Adding About page

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Providers from 'components/layout/Providers'
+import Nav from 'components/layout/Nav/Nav'
+
+const AboutPage = () => {
+  return (
+    <Providers>
+      <Nav />
+    </Providers>
+  )
+}
+
+export default AboutPage
