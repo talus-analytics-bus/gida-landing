171025cf66da10f28a21b4ab92a89c2f5253d4c0
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-06-09 19:55:02 +0000

    fixing icons query hook

--- src/cmsHooks/useIconsQuery.ts
+++ src/cmsHooks/useIconsQuery.ts
@@ -2,25 +2,26 @@ import { useStaticQuery, graphql } from 'gatsby'
 import { AirtableCMSData } from '@talus-analytics/library.airtable-cms'
 
 const useIconsQuery = () => {
-  const iconsQuery = useStaticQuery<AirtableCMSData>(graphql`
-    query iconsQuery {
-      iconsQuery: allAirtable(filter: { table: { eq: "Icons" } }) {
-        nodes {
-          data {
-            Name
-            Text
-            SVG {
-              localFiles {
-                childSvg {
-                  svgString
+  const { iconsQuery }: { iconsQuery: AirtableCMSData } =
+    useStaticQuery(graphql`
+      query iconsQuery {
+        iconsQuery: allAirtable(filter: { table: { eq: "Icons" } }) {
+          nodes {
+            data {
+              Name
+              Text
+              SVG {
+                localFiles {
+                  childSvg {
+                    svgString
+                  }
                 }
               }
             }
           }
         }
       }
-    }
-  `)
+    `)
 
   return iconsQuery
 }
